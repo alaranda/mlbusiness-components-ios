9f182cbd7de42be1be04d3b0657704a63694a0af
alaranda <alexis.aranda@mercadolibre.cl>
2022-02-14 14:26:09 -0300

    feature: add deeplink variable to banner data

--- Source/Components/Banner/MLBusinessAdsBannerData.swift
+++ Source/Components/Banner/MLBusinessAdsBannerData.swift
@@ -9,6 +9,7 @@ import Foundation
 
 @objc public protocol MLBusinessAdsBannerData: NSObjectProtocol {
     @objc optional func getTitle() -> String?
+    @objc func getDeepLink() -> String
     @objc optional func getSubtitle() -> String?
     @objc func getUrl() -> String
 
